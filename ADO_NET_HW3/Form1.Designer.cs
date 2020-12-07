9821b899673edefbc3cb9efc89e0070221d8616f
Bartash Yriy <66261160+uyriy18@users.noreply.github.com>
2020-12-07 22:55:04 +0200

    Добавьте файлы проекта.

---
+++ ADO_NET_HW3/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace ADO_NET_HW3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txbx_FirstStream = new System.Windows.Forms.TextBox();
+            this.txbx_SecondStream = new System.Windows.Forms.TextBox();
+            this.btn_Start = new System.Windows.Forms.Button();
+            this.txbx_Result = new System.Windows.Forms.TextBox();
+            this.lbl_Info = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txbx_FirstStream
+            // 
+            this.txbx_FirstStream.Location = new System.Drawing.Point(12, 42);
+            this.txbx_FirstStream.Name = "txbx_FirstStream";
+            this.txbx_FirstStream.ReadOnly = true;
+            this.txbx_FirstStream.Size = new System.Drawing.Size(298, 20);
+            this.txbx_FirstStream.TabIndex = 0;
+            // 
+            // txbx_SecondStream
+            // 
+            this.txbx_SecondStream.Location = new System.Drawing.Point(12, 85);
+            this.txbx_SecondStream.Name = "txbx_SecondStream";
+            this.txbx_SecondStream.ReadOnly = true;
+            this.txbx_SecondStream.Size = new System.Drawing.Size(298, 20);
+            this.txbx_SecondStream.TabIndex = 1;
+            // 
+            // btn_Start
+            // 
+            this.btn_Start.BackColor = System.Drawing.Color.Crimson;
+            this.btn_Start.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btn_Start.Location = new System.Drawing.Point(367, 42);
+            this.btn_Start.Name = "btn_Start";
+            this.btn_Start.Size = new System.Drawing.Size(75, 63);
+            this.btn_Start.TabIndex = 2;
+            this.btn_Start.Text = "START";
+            this.btn_Start.UseVisualStyleBackColor = false;
+            this.btn_Start.Click += new System.EventHandler(this.btn_Start_Click);
+            // 
+            // txbx_Result
+            // 
+            this.txbx_Result.Location = new System.Drawing.Point(448, 41);
+            this.txbx_Result.Multiline = true;
+            this.txbx_Result.Name = "txbx_Result";
+            this.txbx_Result.ReadOnly = true;
+            this.txbx_Result.Size = new System.Drawing.Size(179, 64);
+            this.txbx_Result.TabIndex = 3;
+            // 
+            // lbl_Info
+            // 
+            this.lbl_Info.AutoSize = true;
+            this.lbl_Info.Location = new System.Drawing.Point(445, 26);
+            this.lbl_Info.Name = "lbl_Info";
+            this.lbl_Info.Size = new System.Drawing.Size(108, 13);
+            this.lbl_Info.TabIndex = 4;
+            this.lbl_Info.Text = "Result of two streams";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 130);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(615, 220);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(641, 353);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.lbl_Info);
+            this.Controls.Add(this.txbx_Result);
+            this.Controls.Add(this.btn_Start);
+            this.Controls.Add(this.txbx_SecondStream);
+            this.Controls.Add(this.txbx_FirstStream);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txbx_FirstStream;
+        private System.Windows.Forms.TextBox txbx_SecondStream;
+        private System.Windows.Forms.Button btn_Start;
+        private System.Windows.Forms.TextBox txbx_Result;
+        private System.Windows.Forms.Label lbl_Info;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ ADO_NET_HW3/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ADO_NET_HW3
+{
+    public partial class Form1 : Form
+    {
+        static int fsCount;
+        static int ssCount;
+        SqlConnection connection;
+        DataTable table;
+        DataSet set;
+        string cs = ConfigurationManager.ConnectionStrings["Liberary"].ConnectionString;
+        public Form1()
+        {
+            InitializeComponent();
+            txbx_FirstStream.Text = "SELECT * FROM Books";
+            txbx_SecondStream.Text = "SELECT * FROM Authors";
+        }
+
+        private void btn_Start_Click(object sender, EventArgs e)
+        {
+            const string AsyncEnabled = "Asynchronous processing = true";
+            if (!cs.Contains(AsyncEnabled))
+            {
+                String.Format("{0};{1}", cs, AsyncEnabled);
+            }
+            using(connection = new SqlConnection(cs))
+            {
+                try
+                {
+                    SqlCommand command = connection.CreateCommand();
+                    command.CommandText = txbx_FirstStream.Text;
+                    command.CommandType = CommandType.Text;
+                    command.CommandTimeout = 30;
+
+                    connection.Open();
+                    IAsyncResult ia = command.BeginExecuteReader();
+                    WaitHandle handle = ia.AsyncWaitHandle;
+
+                    if (handle.WaitOne(5000))
+                    {
+                        GetData(command, ia);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Timeout exedeed");
+                    }
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show(ex.Message, Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            
+            }
+        }
+
+        void GetData(SqlCommand cmd, IAsyncResult ia)
+        {
+            SqlDataReader reader = null;
+            try
+            {
+                string letters = "";
+                reader = cmd.EndExecuteReader(ia);
+                table = new DataTable();
+                bool isHeader = true;
+                do
+                {
+                    while (reader.Read())
+                    {
+                        if (isHeader)
+                        {
+                            for (int i = 0; i < reader.FieldCount; i++)
+                            {
+                                table.Columns.Add(reader.GetName(i));
+                            }
+                            isHeader = false;
+                        }
+                        DataRow row = table.NewRow();
+                        for (int i = 0; i < reader.FieldCount; i++)
+                        {
+                            row[i] = reader[i];
+                            
+                        }
+                        letters += row["Title"];
+                        table.Rows.Add(row);
+                        dataGridView1.DataSource = table;
+                        letters = letters.Trim();
+                        fsCount = letters.Length;
+                        txbx_Result.Text = $"{fsCount} symbols in the all Book's titeles ";
+                    }
+                }
+                while (reader.NextResult());
+
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                if (!reader.IsClosed)
+                {
+                    reader.Close();
+                }
+            }
+        }
+    }
+}
